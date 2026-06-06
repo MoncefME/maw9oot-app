a36c33b5b5ea6e1036401c3859384a0a92fd8016
MoncefME <moncef.m.dev@gmail.com>
2026-06-06 13:33:15 +0200

    update ci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.maw9oot"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
